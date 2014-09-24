a5bd0ef1fb846799dd319696b12abb19ee8c56b2
davidpng <david.p.ng@gmail.com>
2014-09-24 10:08:49 -0700

    new find copy

--- HEADER_Find_Copy_FCS.py
+++ HEADER_Find_Copy_FCS.py
@@ -7,7 +7,7 @@ import shutil
 
 __author__ = "David Ng, MD"
 __copyright__ = "Copyright 2014, David Ng"
-__license__ = "GPL v3"
+__license__ = "GPL"
 __version__ = "1.0"
 __maintainer__ = "David Ng"
 __email__ = "ngdavid@uw.edu"
@@ -25,29 +25,32 @@ class Find_and_Copy_FCS(object):
                 'number_of_cases_found'
                 'number_of_files_transfered' - work in progress
     """
-    def __init__(self,source,target,file_list_name,include_all=False):
+    def __init__(self,source,target,file_list_name,verbose=False):
         self.target_dir = target
         self.source_dir = source
         self.case_list_path = file_list_name
-        self.include_all = include_all
+        self.verbose = verbose
         
         self._make_target_dir()
         self.file_list = self._load_file_list()
+        if self.verbose:
+            print self.file_list
         self.cases_not_found = self._search_for_cases()
-        
+        if self.verbose:
+            print self.cases_not_found
     def _make_target_dir(self):
-        """Make Target Director"""
+        #Make Target Director
         if not os.path.exists(self.target_dir):
             os.makedirs(self.target_dir)
         return None
     
     def _load_file_list(self):
-        """Load list of files to find"""
+        #Load list of files to find
         file_list=[]
-        file = open(self.case_list_path,'r')
+        file = open(self.case_list_path,'rU')
         for name in file:
             if name:    # if line is empty or blank, skip it
-                file_list.append(name.strip())
+                file_list.append(name.strip().strip('HP'))
         file.close()
         return file_list
 
@@ -59,9 +62,9 @@ class Find_and_Copy_FCS(object):
             for _f in files:
                 matches = [l for l in file_list_tracking if l in _f]
                 if any(matches):
-                    print "Source: "+root
-                    print "Target: "+target_dir+"/"+matches[0]
-                    
+                    if self.verbose:
+                        print "Source: "+root
+                        print "Target: "+target_dir+"/"+matches[0]
                     shutil.copytree(root,target_dir+"/"+matches[0],
                                     ignore = self._ignore_except ) #this will copy all files over
                     file_list_tracking.remove(matches[0])
@@ -78,8 +81,7 @@ class Find_and_Copy_FCS(object):
                 'number_of_files_transfered' : 'work in progress'}
 
 if __name__ == '__main__':
-    source_dir = "/Volumes/FlowCyto/LISTMODE/LSR data/Leukemia-Lymphoma/14-17200-17299"
-    target_dir = "/Users/davidng/Desktop/Examples"
-    file_list_name = "/Users/davidng/Documents/PYTHON/Input_Output/cases_to_find.txt"
-    t=Find_and_Copy_FCS(source_dir,target_dir,file_list_name)
-    print t.cases_not_found
+    source_dir = "/Volumes/archive/2010 archived flow files"
+    target_dir = "/Users/davidng/Desktop/Examples/Toy"
+    file_list_name = "/Users/davidng/Documents/PYTHON/Input_Output/Toy_HP_Cases.txt"
+    t=Find_and_Copy_FCS(source_dir,target_dir,file_list_name,verbose=True)
